ca2ce3892e2eab8484d739a49f93db4678cc5844
unknown <kmcdermott31@gmail.com>
2020-08-09 22:15:44 +0100

    fixed anchor issues

--- src/App.js
+++ src/App.js
@@ -17,18 +17,18 @@ function App() {
               <p className="klarna-text">Tel: 0800 107 8665 / 01698 819 000</p>
             </div>
             <div className="fake-buttons">
-              <a>About Us</a>
-              <a>Blog</a>
-              <a>Customer Services</a>
-              <a>Track Order</a>
-              <a>Currency</a>
+              <a href="/">About Us</a>
+              <a href="/">Blog</a>
+              <a href="/">Customer Services</a>
+              <a href="/">Track Order</a>
+              <a href="/">Currency</a>
             </div>
           </div>
           <div className="klarna">
             <p className="klarna-text">3 months interest-free payments with <strong>Klarna</strong></p>
           </div>
           <div className="header-bar">
-            <a href="/"><img src="https://cdn11.bigcommerce.com/s-7holhynnib/images/stencil/250x100/toolstop_logo_1582119093__49512.original.png" /></a>
+            <a href="/"><img src="https://cdn11.bigcommerce.com/s-7holhynnib/images/stencil/250x100/toolstop_logo_1582119093__49512.original.png" alt=""/></a>
             <input type="text" className="search-box" placeholder="Search product or brand..." />
             <button type="button" className="sign-in">Sign In</button>
           </div>
